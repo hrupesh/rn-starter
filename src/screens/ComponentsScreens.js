ff9da25b807aeea9eeaf0806bbd25c90845735ed
hrupesh <rcashok0@gmail.com>
2020-06-15 04:36:48 +0530

    ...

--- src/screens/ComponentsScreens.js
+++ src/screens/ComponentsScreens.js
@@ -2,16 +2,26 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function ComponentsScreens() {
+  var name = "Rupesh Chaudhari";
+
   return (
     <View>
-      <Text style={styles.text}>This a Component.</Text>;
+      <Text style={styles.text}>Getting started with react native!</Text>
+      <Text style={styles.text2}>My Name is {name}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30,
+    fontSize: 45,
+    letterSpacing: 2,
+    paddingHorizontal: 20,
+    padding: 10,
+  },
+  text2: {
+    fontSize: 20,
+    color: '#536DFE',
     letterSpacing: 2,
     paddingHorizontal: 20,
     padding: 10,
---
+++ src/screens/ListScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function ListScreen() {
+    return (
+        <View>
+            <Text>List Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
