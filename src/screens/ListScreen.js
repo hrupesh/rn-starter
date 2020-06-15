3a7b5c6ab20b8a15a74d51f874443f8dd8d1be37
hrupesh <rcashok0@gmail.com>
2020-06-15 20:44:07 +0530

    .....

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,12 +1,48 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
 export default function ListScreen() {
-    return (
-        <View>
-            <Text>List Screen</Text>
-        </View>
-    )
+  const things = [
+    { thing: "Thing 1" },
+    { thing: "Thing 2" },
+    { thing: "Thing 3" },
+    { thing: "Thing 4" },
+    { thing: "Thing 5" },
+    { thing: "Thing 6" },
+    { thing: "Thing 7" },
+    { thing: "Thing 8" },
+    { thing: "Thing 9" },
+    { thing: "Thing 10" },
+    { thing: "Thing 11" },
+  ];
+
+  return (
+    <View>
+      <Text style={styles.headingtext}>List Screen</Text>
+      <FlatList
+        keyExtractor={thing => thing.thing}
+        data={things}
+        renderItem={({item}) => {
+          return <Text style={styles.ListText} > {item.thing} </Text>;
+        }}
+      />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  headingtext: {
+    fontSize: 30,
+    textAlign: "center",
+    padding: 10,
+  },
+  ListText:{
+      fontSize:24,
+      letterSpacing:2,
+      fontFamily:'monospace',
+      textAlign:'center',
+      backgroundColor:'#fff',
+      padding:10,
+      margin:5
+  }
+});
