a9655075219fa75814aa2c5e79d088cea0d01fd5
hrupesh <rcashok0@gmail.com>
2020-06-17 00:09:14 +0530

    ....

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,65 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}> Home </Text>;
+const HomeScreen = ({navigation}) => {
+  const goToComponets = () => {
+    navigation.navigate("Components");
+  };
+
+  const goToList = () => {
+    navigation.navigate("List");
+  };
+
+  return (
+    <View style={styles.view}>
+      <TouchableOpacity onPress={goToComponets} style={styles.btn}>
+        <Text style={styles.btntext}>Component Screen</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={goToList} style={styles.btn2}>
+        <Text style={styles.btntext}>List Screen</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
-    padding:10,
-    paddingHorizontal:20
+    padding: 10,
+    paddingHorizontal: 20,
+  },
+  view: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btn: {
+    width: "90%",
+    height: "10%",
+    borderRadius: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#2196F3",
+    textAlign: "center",
+    margin: 15,
+  },
+  btn2: {
+    width: "90%",
+    height: "10%",
+    borderRadius: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#448AFF",
+    textAlign: "center",
+    margin: 15,
+  },
+  btntext: {
+    textAlign: "center",
+    fontFamily: "monospace",
+    fontSize: 20,
+    color: "white",
+    letterSpacing: 5,
   },
 });
 
