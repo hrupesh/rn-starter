5c942d13c962cfb4ff069188f8996c7a01e637c2
hrupesh <rcashok0@gmail.com>
2020-06-17 16:14:25 +0530

    ....

--- src/components/ImgCard.js
+++ src/components/ImgCard.js
@@ -1,12 +1,53 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
-export default function ImgCard() {
-    return (
-        <View>
-            <Text>Image Card</Text>
-        </View>
-    )
+export default function ImgCard(props) {
+  return (
+    <TouchableOpacity activeOpacity={0.8} style={styles.card}>
+      {/* <Feather name="image" size={75} color="white" /> */}
+      <Image
+        fadeDuration={100}
+        style={styles.img}
+        source={{ uri: props.imgurl }}
+      />
+      <Text numberOfLines={1} style={styles.cardText}>
+        {props.item}
+      </Text>
+    </TouchableOpacity>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  card: {
+    width: "95%",
+    height: 200,
+    backgroundColor: "#2196f3",
+    alignSelf: "flex-start",
+    borderRadius: 50,
+    marginVertical: 10,
+    borderLeftWidth: 10,
+    borderLeftColor: "#0d47a1",
+    borderTopLeftRadius: 0,
+    borderBottomLeftRadius: 0,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    padding: 25,
+  },
+  img: {
+    height: "100%",
+    width: "35%",
+    borderRadius: 5,
+  },
+  cardText: {
+    width: "65%",
+    fontSize: 22,
+    fontFamily: "monospace",
+    paddingHorizontal: 25,
+    color: "white",
+    fontWeight: "100",
+    letterSpacing: 2,
+    overflow: "hidden",
+  },
+});
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import ImgCard from '../components/ImgCard'
-
-
+import React from "react";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import ImgCard from "../components/ImgCard";
 
 export default function ImageScreen() {
-    return (
-        <View>
-            <ImgCard></ImgCard>
-            <ImgCard></ImgCard>
-            <ImgCard></ImgCard>
-            <ImgCard></ImgCard>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <ImgCard imgurl="https://picsum.photos/400/400" item="Item 1"></ImgCard>
+      <ImgCard imgurl="https://picsum.photos/450/450" item="Item 2"></ImgCard>
+      <ImgCard imgurl="https://picsum.photos/400" item="Item 3"></ImgCard>
+      <ImgCard imgurl="https://picsum.photos/450" item="Item 4"></ImgCard>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+  },
+});
