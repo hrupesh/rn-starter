e2e54cdcd743e7e3e6a559be476b559ceed70036
hrupesh <rcashok0@gmail.com>
2020-06-21 04:22:45 +0530

    Adding Counter Screen

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,12 +1,38 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function CounterScreen() {
-    return (
-        <View>
-            <Text>Counter Screen</Text>
-        </View>
-    )
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+      setCount(count +1);
+  }
+
+  const decreaseCount = () => {
+      setCount(count - 1);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Counter Screen</Text>
+      <TouchableOpacity onPress={increaseCount}>
+        <AntDesign name="pluscircle" size={40} color="white" />
+      </TouchableOpacity>
+      <Text> {count} </Text>
+      <TouchableOpacity onPress={decreaseCount}>
+        <AntDesign name="minuscircle" size={40} color="white" />
+      </TouchableOpacity>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#212121",
+  },
+});
