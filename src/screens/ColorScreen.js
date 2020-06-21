6fbe5769990f49804d7118209066246083570495
hrupesh <rcashok0@gmail.com>
2020-06-22 01:00:57 +0530

    Added new Navbar

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,13 +1,35 @@
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
-import { TouchableNativeFeedback } from "react-native-gesture-handler";
+import {
+  TouchableNativeFeedback,
+  ScrollView,
+} from "react-native-gesture-handler";
 
 export default function ColorScreen() {
+  const randomRGB = () => {
+    const red = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+
+    return `rgb(${red}, ${blue} , ${green})`;
+  };
+
   return (
     <View>
       <Button title="Add Color" />
+      <ScrollView style={styles.container}>
+        <View
+          style={{ height: 100, width: 100, backgroundColor: randomRGB() }}
+        ></View>
+      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    width: "100%",
+    overflow: "scroll",
+  },
+});
