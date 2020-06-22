ecb8b2f92478348ee1b8a6ccc33cdf958d0b005d
hrupesh <rcashok0@gmail.com>
2020-06-23 02:28:34 +0530

    Added Random Color Generator Component

--- src/screens/ColorCounter.js
+++ src/screens/ColorCounter.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 
-export default function ColorCounter({ color }) {
+export default function ColorCounter({ color, onAdd, onReduce }) {
   return (
     <View>
       <Text> {color} </Text>
-      <Button title={`Add ${color}`} />
-      <Button title={`Reduce ${color}`} />
+      <Button title={`Add ${color}`} onPress={() => onAdd()} />
+      <Button title={`Reduce ${color}`} onPress={() => onReduce()} />
     </View>
   );
 }
--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -12,20 +12,32 @@ export default function SquareScreen() {
       <Text>Square Screen</Text>
 
       <ColorCounter
-        onAdd={() => setRed(red + 1)}
-        onReduce={() => setRed(red - 1)}
+        onAdd={() => setRed(red + 100)}
+        onReduce={() => setRed(red - 100)}
         color="Red"
       />
       <ColorCounter
-        onAdd={() => setgreen(green + 1)}
-        onReduce={() => setgreen(green - 1)}
+        onAdd={() => setgreen(green + 100)}
+        onReduce={() => setgreen(green - 100)}
         color="Green"
       />
       <ColorCounter
-        onAdd={() => setblue(blue + 1)}
-        onReduce={() => setblue(blue - 1)}
+        onAdd={() => setblue(blue + 100)}
+        onReduce={() => setblue(blue - 100)}
         color="Blue"
       />
+
+      <Text>
+        {" "}
+        Current Value : RGB({red}, {green}, {blue}){" "}
+      </Text>
+      <View
+        style={{
+          height: "100%",
+          width: "100%",
+          backgroundColor: `rgb(${red},${green},${blue})`,
+        }}
+      ></View>
     </View>
   );
 }
