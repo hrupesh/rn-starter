14cda0704a49f83c536a3737a3ad0c8788c0b3e1
hrupesh <rcashok0@gmail.com>
2020-06-23 01:53:08 +0530

    Added Random Color Generator Component

--- src/screens/ColorCounter.js
+++ src/screens/ColorCounter.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
-export default function ColorCounter({color}) {
-    return (
-        <View>
-            <Text> {color} </Text>
-            <Button title="Add {color}" />
-            <Button title="Reduce {color}" />
-        </View>
-    )
+export default function ColorCounter({ color }) {
+  return (
+    <View>
+      <Text> {color} </Text>
+      <Button title="Add " />
+      <Button title="Reduce" />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
