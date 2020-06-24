70c208bbe6531cec5dadeb4950cdc8ec9627a0a0
hrupesh <rcashok0@gmail.com>
2020-06-24 22:55:11 +0530

    Added Create Colors Component

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -19,44 +19,44 @@ const reducer = (state, action) => {
 export default function SquareScreen() {
   const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
 
-//   const validateColors = () => {
-//     if (red > 255) {
-//       setRed(255);
-//     } else if (red < 0) {
-//       setRed(0);
-//     }
+  //   const validateColors = () => {
+  //     if (red > 255) {
+  //       setRed(255);
+  //     } else if (red < 0) {
+  //       setRed(0);
+  //     }
 
-//     if (green > 255) {
-//       setgreen(255);
-//     } else if (green < 0) {
-//       setgreen(0);
-//     }
+  //     if (green > 255) {
+  //       setgreen(255);
+  //     } else if (green < 0) {
+  //       setgreen(0);
+  //     }
 
-//     if (blue > 255) {
-//       setblue(255);
-//     } else if (blue < 0) {
-//       setblue(0);
-//     }
-//   };
+  //     if (blue > 255) {
+  //       setblue(255);
+  //     } else if (blue < 0) {
+  //       setblue(0);
+  //     }
+  //   };
 
-//   validateColors();
+  //   validateColors();
 
   return (
     <View style={styles.container}>
       <ColorCounter
-        onAdd={() => dispatch({setColor:'red',amount:THRESHOLD})}
-        onReduce={() => dispatch({setColor:'red',amount:-(THRESHOLD)})}
+        onAdd={() => dispatch({ setColor: "red", amount: THRESHOLD })}
+        onReduce={() => dispatch({ setColor: "red", amount: -THRESHOLD })}
         color="Red"
       />
       <ColorCounter
-        onAdd={() => dispatch({setColor:'blue',amount:THRESHOLD})}
-        onReduce={() => dispatch({setColor:'blue',amount:-(THRESHOLD)})}
-        color="Blue"
+        onAdd={() => dispatch({ setColor: "green", amount: THRESHOLD })}
+        onReduce={() => dispatch({ setColor: "green", amount: -THRESHOLD })}
+        color="Green"
       />
       <ColorCounter
-        onAdd={() => dispatch({setColor:'green',amount:THRESHOLD})}
-        onReduce={() => dispatch({setColor:'green',amount:-(THRESHOLD)})}
-        color="Green"
+        onAdd={() => dispatch({ setColor: "blue", amount: THRESHOLD })}
+        onReduce={() => dispatch({ setColor: "blue", amount: -THRESHOLD })}
+        color="Blue"
       />
 
       <Text style={styles.curVal}>
