b4f8b77ef9328f9dd7b19ba0bcdf8e468e5e7ece
hrupesh <rcashok0@gmail.com>
2020-06-24 22:51:44 +0530

    Added Create Colors Component

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,51 +1,61 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import ColorCounter from "./ColorCounter";
 
 const THRESHOLD = 5;
 
+const reducer = (state, action) => {
+  switch (action.setColor) {
+    case "red":
+      return { ...state, red: state.red + action.amount };
+    case "green":
+      return { ...state, green: state.green + action.amount };
+    case "blue":
+      return { ...state, blue: state.blue + action.amount };
+    default:
+      return state;
+  }
+};
 export default function SquareScreen() {
-  const [red, setRed] = useState(0);
-  const [green, setgreen] = useState(0);
-  const [blue, setblue] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
 
-  const validateColors = () => {
-    if (red > 255) {
-      setRed(255);
-    } else if (red < 0) {
-      setRed(0);
-    }
+//   const validateColors = () => {
+//     if (red > 255) {
+//       setRed(255);
+//     } else if (red < 0) {
+//       setRed(0);
+//     }
 
-    if (green > 255) {
-      setgreen(255);
-    } else if (green < 0) {
-      setgreen(0);
-    }
+//     if (green > 255) {
+//       setgreen(255);
+//     } else if (green < 0) {
+//       setgreen(0);
+//     }
 
-    if (blue > 255) {
-      setblue(255);
-    } else if (blue < 0) {
-      setblue(0);
-    }
-  };
+//     if (blue > 255) {
+//       setblue(255);
+//     } else if (blue < 0) {
+//       setblue(0);
+//     }
+//   };
 
-  validateColors();
+//   validateColors();
 
   return (
     <View style={styles.container}>
       <ColorCounter
-        onAdd={() => setRed(red + THRESHOLD)}
-        onReduce={() => setRed(red - THRESHOLD)}
+        onAdd={() => dispatch({setColor:'red',amount:THRESHOLD})}
+        onReduce={() => dispatch({setColor:'red',amount:-(THRESHOLD)})}
         color="Red"
       />
       <ColorCounter
-        onAdd={() => setgreen(green + THRESHOLD)}
-        onReduce={() => setgreen(green - THRESHOLD)}
+        onAdd={() => dispatch({setColor:'blue',amount:THRESHOLD})}
+        onReduce={() => dispatch({setColor:'blue',amount:-(THRESHOLD)})}
         color="Green"
       />
       <ColorCounter
-        onAdd={() => setblue(blue + THRESHOLD)}
-        onReduce={() => setblue(blue - THRESHOLD)}
+        onAdd={() => dispatch({setColor:'green',amount:THRESHOLD})}
+        onReduce={() => dispatch({setColor:'green',amount:-(THRESHOLD)})}
         color="Blue"
       />
 
@@ -67,11 +77,11 @@ const styles = StyleSheet.create({
   container: {
     textAlign: "center",
   },
-  curVal:{
-      fontSize: 18,
-      letterSpacing:1,
-      textAlign:'center',
-      backgroundColor:'#212121',
-      color:'white'
-  }
+  curVal: {
+    fontSize: 18,
+    letterSpacing: 1,
+    textAlign: "center",
+    backgroundColor: "#212121",
+    color: "white",
+  },
 });
