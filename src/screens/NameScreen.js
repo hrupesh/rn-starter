01d1048fdc5ad8b2e5e68b3a297717d6094e9837
hrupesh <rcashok0@gmail.com>
2020-06-25 22:39:05 +0530

    Impleted counter with reducer

--- src/screens/NameScreen.js
+++ src/screens/NameScreen.js
@@ -1,24 +1,44 @@
-import React from 'react'
-import { StyleSheet, Text, View, TextInput } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function NameScreen() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.label}>Enter Text</Text>
-            <TextInput  placeholder="Enter Text"  style={styles.input}/>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Enter Text</Text>
+      <TextInput placeholder="Enter Text" style={styles.input} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        alignItems:'center',
-        justifyContent:'center',
-        fontFamily:'Nunito Sans Regular'
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    fontFamily: "Nunito Sans Regular",
+  },
+  label: {
+    fontSize: 35,
+    letterSpacing: 2,
+  },
+  input: {
+    height: 90,
+    width: "75%",
+    fontSize: 24,
+    margin:15,
+    paddingHorizontal:25,
+    // borderBottomWidth: 1,
+    // borderBottomColor: "#aaa",
+    color: "#212121",
+    borderRadius: 50,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 5,
+      height: 5,
     },
-    label:{
-        fontSize:35,
-        letterSpacing:2,
-    }
-})
+    elevation:10,
+    shadowOpacity:0.8,
+    shadowRadius:50,
+    backgroundColor:'#ddd'
+  },
+});
