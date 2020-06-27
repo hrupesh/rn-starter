ae8cad273b405ff694e56d5a07d46dd3eac917d9
hrupesh <rcashok0@gmail.com>
2020-06-27 22:38:56 +0530

    Changing UI

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,10 +3,19 @@ import { StyleSheet, Text, View } from 'react-native'
 
 export default function BoxScreen() {
     return (
-        <View>
-            <Text>Box Screen</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Box Screen</Text>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        borderWidth:4,
+        borderColor:'yellow'
+    },
+    text:{
+        borderColor:'tomato',
+        borderWidth:4
+    }
+})
--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-import { StyleSheet, Text, View, Alert } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { AntDesign } from "@expo/vector-icons";
 
