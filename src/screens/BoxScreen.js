a4d16e14ff1aff2bfdf7d6306d85fc8d2d4e606a
hrupesh <rcashok0@gmail.com>
2020-06-27 23:28:51 +0530

    Changing UI

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,10 +4,10 @@ import { StyleSheet, Text, View } from 'react-native'
 export default function BoxScreen() {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Box Screen</Text>
-            <Text style={styles.text}>Box Screen</Text>
-            <Text style={styles.text}>Box Screen</Text>
-            <Text style={styles.text}>Box Screen</Text>
+            <Text style={styles.text1}>Box Screen</Text>
+            <Text style={styles.text2}>Box Screen</Text>
+            <Text style={styles.text3}>Box Screen</Text>
+            <Text style={styles.text4}>Box Screen</Text>
         </View>
     )
 }
@@ -16,14 +16,33 @@ const styles = StyleSheet.create({
     container:{
         borderWidth:4,
         borderColor:'yellow',
-        alignItems:'center',
-        flexDirection:'row',
-        justifyContent:'center'
+        // alignItems:'center',
+        // flexDirection:'row',
+        justifyContent:'center',
+        height:'100%'
     },
-    text:{
+    text1:{
+        borderColor:'tomato',
+        borderWidth:4,
+        // flex:0,
+        textAlign:"center"
+    },
+    text2:{
+        borderColor:'tomato',
+        borderWidth:4,
+        flex:1,
+        textAlign:"center"
+    },
+    text3:{
         borderColor:'tomato',
         borderWidth:4,
         // flex:0.2,
         textAlign:"center"
-    }
+    },
+    text4:{
+        borderColor:'tomato',
+        borderWidth:4,
+        // flex:0.2,
+        textAlign:"center"
+    },
 })
